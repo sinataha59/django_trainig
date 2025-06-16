853ccc1cf41dab235836a3f2d2de4dafa1970c6d
sina <sina.backend@gmail.com>
2025-06-16 18:54:32 +0430

    updating food.models

--- django/my_store/food/models.py
+++ django/my_store/food/models.py
@@ -8,7 +8,11 @@ class product(models.Model):
     datetime_modified = models.DateTimeField( auto_now=True, auto_now_add=False)
     inventory = models.IntegerField(default =0)
     price = models.DecimalField( max_digits=6, decimal_places=2)
+    category = models.ForeignKey(category)
 
+class category (models.Model) :
+    title = models.CharField(max_length = 250)
+    discription = models.SlugField(blank = True)
 
 
 class customer(models.Model):
@@ -25,15 +29,21 @@ class order(models.Model):
     CANCELEDORDER = 'C'
     
     ORDERSTATUS =[
+        
         (PAIDORDER,"paid"),
         (UNPAIDORDER,"unpaid"),
         (CANCELEDORDER,"canceled"),
-        
-        
+       
     ]
     
-    customer_name = models.CharField(max_length = 50)
-    datetime_creat =models.DateTimeField(auto_now =False , auto_now_add = True) 
-    status =models.CharField(max_length = 255 , choices = ORDERSTATUS , default = UNPAIDORDER)
-     
-    
+    customer_name = models.ForeignKey(customer)
+    datetime_creat = models.DateTimeField(auto_now =False , auto_now_add = True) 
+    status = models.CharField(max_length = 255 , choices = ORDERSTATUS , default = UNPAIDORDER)
+    product = models.ForeignKey(product)
+ 
+ 
+class coment(models.Model):
+     name = models.ForeignKey(customer)
+     text = models.models.CharField( max_length=500)
+     datetime_created = models.DateTimeField(auto_now_add =True)
+     product = models.ForeignKey(product)
